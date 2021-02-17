1cfedb2807b27304425971715670d802d1ca7cf6
daniel-n6 <54150946+daniel-n6@users.noreply.github.com>
2021-02-17 16:54:16 -0600

    modified nav bar

--- lib/views/widgets/navigation_bar.dart
+++ lib/views/widgets/navigation_bar.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 class NavigationBar extends StatelessWidget {
   final Widget _body;
   final String _title;
-  NavigationBar({String title, Widget body}) : 
-  _body = body,
-  _title = title;
+  NavigationBar({String title, Widget body})
+      : _body = body,
+        _title = title;
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +13,35 @@ class NavigationBar extends StatelessWidget {
       headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
         return <Widget>[
           SliverAppBar(
-            title: Center(child: Text(_title)),
+            automaticallyImplyLeading: false,
+            title: Container(
+              constraints: BoxConstraints(maxWidth: 800),
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                      IconButton(onPressed: () {
+                        Navigator.pop(context);
+                      }, icon: Icon(Icons.arrow_back))
+                    ]),
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                      ElevatedButton(onPressed: () {}, child: Text("Sign In")),
+                    ]),
+                  ),
+                  Text(_title),
+                ],
+              ),
+            ),
+            centerTitle: true,
             floating: true,
           ),
         ];
