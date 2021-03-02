eed605ba7df669f77ff944e76bb919f28994ac3e
daniel-n6 <54150946+daniel-n6@users.noreply.github.com>
2021-03-02 13:41:13 -0600

    displays courses from cloud firestore

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:quizzywizzy/views/widgets/navigation_bar.dart';
 import 'package:quizzywizzy/views/widgets/selection_cell.dart';
 import 'package:quizzywizzy/constants.dart' as Constants;
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomeView extends StatelessWidget {
   @override
@@ -17,36 +18,46 @@ class HomeView extends StatelessWidget {
               alignment: Alignment.topCenter,
               child: Container(
                 constraints: BoxConstraints(maxWidth: 600),
-                child: ListView(
-                  children: [
-                    Center(
-                        child: Container(
-                      padding: EdgeInsets.symmetric(vertical: 40),
-                      child: Text("Select a course",
-                          style: TextStyle(
-                            fontSize: 40,
-                          )),
-                    )),
-                    GridView(
-                      shrinkWrap: true,
-                      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                        maxCrossAxisExtent: 150,
-                        crossAxisSpacing: 10,
-                        mainAxisSpacing: 10,
-                        childAspectRatio: 1.0,
-                        //maxCrossAxisCount: 5,
-                      ),
-                      children: List.generate(
-                          40,
-                          (index) => SelectionCell(
-                              text: "hi",
-                              icon: Icons.ac_unit,
-                              onTap: () {
-                                Navigator.of(context).pushReplacementNamed(Constants.getCourseRoute(index));
-                              })),
-                    ),
-                  ],
-                ),
+                child: FutureBuilder<QuerySnapshot>(
+                    future: FirebaseFirestore.instance
+                        .collection('courses').get(),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (snapshot.hasData) {
+                        return ListView(
+                          children: [
+                            Center(
+                                child: Container(
+                              padding: EdgeInsets.symmetric(vertical: 40),
+                              child: Text("Select a course",
+                                  style: TextStyle(
+                                    fontSize: 40,
+                                  )),
+                            )),
+                            GridView(
+                              shrinkWrap: true,
+                              gridDelegate:
+                                  SliverGridDelegateWithMaxCrossAxisExtent(
+                                maxCrossAxisExtent: 150,
+                                crossAxisSpacing: 10,
+                                mainAxisSpacing: 10,
+                                childAspectRatio: 1.0,
+                                //maxCrossAxisCount: 5,
+                              ),
+                              children: snapshot.data.docs.map((doc) => SelectionCell(
+                                      text: doc.data()["name"],
+                                      icon: Icons.ac_unit,
+                                      onTap: () {
+                                        Navigator.of(context)
+                                            .pushReplacementNamed(
+                                                Constants.getCourseRoute(doc.data()["name"]));
+                                      })).toList(),
+                            ),
+                          ],
+                        );
+                      }
+                      return Center(child: CircularProgressIndicator());
+                    }),
               ),
             ),
           ),
