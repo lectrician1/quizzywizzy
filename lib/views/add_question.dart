b2f843f91d442a34965bcce29651cfd597c1a09c
maxdela3 <54151011+maxdela3@users.noreply.github.com>
2021-04-14 21:18:09 -0500

    first

--- lib/views/add_question.dart
+++ lib/views/add_question.dart
@@ -4,6 +4,51 @@ import 'package:quizzywizzy/widgets/body_template.dart';
 class AddQuestionView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BodyTemplate(child: Text("Add Question"));
+   return BodyTemplate(child: MyCustomForm());
+  }
+}
+
+class MyCustomForm extends StatefulWidget {
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+
+class MyCustomFormState extends State<MyCustomForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'greyout static',
+            ),
+          ),
+        ),
+          Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'greyout static',
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextFormField(
+            decoration: InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'greyout dynamic',
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
